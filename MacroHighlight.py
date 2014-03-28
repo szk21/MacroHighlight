52de7df5c209139052806c5e71fcfbe459439254
szk21 <szk21@sina.com>
2014-03-28 15:44:41 +0800

    - Add Typedef key word Type highlight; - Add menu and keymap for platform;

--- MacroHighlight.py
+++ MacroHighlight.py
@@ -8,25 +8,34 @@ class MacroHighlightCommand(sublime_plugin.TextCommand):
 		files = os.listdir(file_path);
 
 		all_data = '\\b(ThisIsMacroList'
+		typedef_data = '\\b(ThisIsTypedefList'
 		for filename in files:
-		    if filename[-2:] == '.h' or filename[-2:] == '.H':
+		    if filename[-2:] == '.h' or filename[-2:] == '.H' or filename[-2:] == '.c' or filename[-4:] == '.cpp':
 		        f = open(file_path + '\\' + filename) 
 		        lines = f.readlines()
 
 		        for line in lines:
-		            data = line.split()
+		            data = line.strip().split()
 		            if not data:
 		                continue
 		            if data[0] == '#define':
 		                all_data = all_data + '|' + data[1].split('(')[0]
+		            elif data[0] == 'typedef':
+		            	if data[-1] == ';':
+		            		typedef_data = typedef_data + '|' + data[-2].strip('*();[]')
+		            	elif data[-1][-1] == ';':
+		            		typedef_data = typedef_data + '|' + data[-1].strip('*();[]')
 		        f.close()
 		    
-		all_data = all_data + ')\\b'    
+		all_data = all_data + ')\\b' 
+		typedef_data = typedef_data + ')\\b'   
 
 		xml_file= sublime.packages_path() + '\\C++\\C.tmLanguage'
 		xml=ElementTree.ElementTree(file=xml_file)
 		root = xml.getroot()
 
-		string_node = root.findall('dict')[0].findall('array')[1].find('dict').getchildren()[1]
+		string_node = root.findall('dict')[0].findall('array')[1][0].getchildren()[1];
 		string_node.text = all_data
+		string_node = root.findall('dict')[0].findall('array')[1][1].getchildren()[1];
+		string_node.text = typedef_data
 		xml.write(xml_file,  "UTF-8")
